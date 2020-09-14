c14bf849114362845044b8d7179824d055f99bb4
rfinegan1 <57640306+rfinegan1@users.noreply.github.com>
2020-09-14 13:32:56 -0400

    Add files via upload

---
+++ fe595assignment1_RyanFinegan.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 10:21:15 2020
+
+@author: ryanfinegan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    x = np.arange(0,2*np.pi,.01)
+    y = np.sin(x)
+    z = np.cos(x)
+    plt.plot(x,y,x,z)
+    plt.show()
+    
+if __name__ == '__main__':
+    main()
